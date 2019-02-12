2818d173409ca0782965bbf42b98ffa9e379eac8
admin <admin@DESKTOP-DKPKRBQ.wireless.yale.internal>
2019-02-12 17:35:21 -0500

    siqi's crop tool

--- script/bsc_use_image_crop_tool.m
+++ script/bsc_use_image_crop_tool.m
@@ -1,11 +1,15 @@
 meas = struct();
 meas.inter_eye_dist_cm = 4;
-meas.face_width_cm = 12.4;
-meas.face_height_cm = 11.2;
-meas.face_top_to_eye_center_cm = 3.4;
-meas.face_left_to_eye_center_cm = 6.2;
+%meas.face_width_cm = 12.4;
+meas.face_width_cm = 14.2;
+%meas.face_height_cm = 11.2;
+meas.face_height_cm = 13.1;
+%meas.face_top_to_eye_center_cm = 3.4;
+meas.face_top_to_eye_center_cm = 4.1;
+%meas.face_left_to_eye_center_cm = 6.2;
+meas.face_left_to_eye_center_cm = 7.1;
 
 bsc_image_crop_tool( ...
     'far_plane_measurements', meas ...
-  , 'padding', [0, 0] ...
+  , 'padding', [500, 500] ...
 );
